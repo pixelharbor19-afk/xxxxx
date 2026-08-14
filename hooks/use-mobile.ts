b15017916e7eeba23c6f62d581c448a78c9db32f
jerometecson-main <zxctest8@zxcstream.xyz>
2026-08-14 17:07:33 +0800

    use mobile

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,22 +1,43 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const QUERY = "(max-width: 767px)";
+// const QUERY = "(max-width: 767px)";
+
+// export function useIsMobile() {
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   useEffect(() => {
+//     const media = window.matchMedia(QUERY);
+
+//     const update = () => setIsMobile(media.matches);
+
+//     update();
+//     media.addEventListener("change", update);
+
+//     return () => media.removeEventListener("change", update);
+//   }, []);
+
+//   return isMobile;
+// }
+import * as React from "react";
+
+const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+    undefined,
+  );
 
-  useEffect(() => {
-    const media = window.matchMedia(QUERY);
-
-    const update = () => setIsMobile(media.matches);
-
-    update();
-    media.addEventListener("change", update);
-
-    return () => media.removeEventListener("change", update);
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+    mql.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
   }, []);
 
-  return isMobile;
+  return !!isMobile;
 }
