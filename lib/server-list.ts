49314014d414bf2c84f5cb7274b39b23ae6ac124
jerometecson-main <zxctest8@zxcstream.xyz>
2026-08-17 12:34:38 +0800

    resshin server active

--- lib/server-list.ts
+++ lib/server-list.ts
@@ -20,17 +20,16 @@ export const initialServers: ServerTypes[] = [
     desc: "Download & Multi Audio Support",
   },
   {
-    name: "Berkas III",
-    server: "1berkas",
-    status: "queue",
-    desc: "4K Support & Fast",
-  },
-
-  {
-    name: "Resshin IV",
+    name: "Resshin III",
     server: "1resshin",
     status: "queue",
     desc: "Alternative Multi Audio Support",
+  },
+  {
+    name: "Berkas IV",
+    server: "1berkas",
+    status: "queue",
+    desc: "4K Support & Fast",
   },
 
   // {
