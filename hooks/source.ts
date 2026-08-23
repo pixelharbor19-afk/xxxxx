3ac0eeb9f5466afbd807a7fb2b59b3439c7b6143
jerometecson-main <zxctest8@zxcstream.xyz>
2026-08-24 05:02:52 +0800

    zxc

--- hooks/source.ts
+++ hooks/source.ts
@@ -92,7 +92,7 @@ export default function useSource(params: UseSourceParams) {
     refetchIntervalInBackground: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend_/dynamic-token", {
+      const { data: token } = await axios.post("/backend_/token", {
         id: tmdbId,
         media_type,
         path,
