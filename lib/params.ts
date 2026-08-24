77d6b03e94cba2c8ea51ab33682e77270d3383e4
jerometecson-main <zxctest8@zxcstream.xyz>
2026-08-24 20:52:42 +0800

    more workers and rotate

--- lib/params.ts
+++ lib/params.ts
@@ -4,18 +4,18 @@ import { SALT } from "./salt";
 // 🔁 Rotate these constants every few weeks
 
 const FIELD_MAP = {
-  id: "c81f42d9e2254264316f408",
-  fToken: "9e3c7b134af457652481d0e493",
-  ts: "54d8b2681fa37e260b1fd",
-  token: "b7f18e4c225d63aef81c4a9",
-  title: "2af9c71de3228480546391e",
-  year: "f0b34e866a22909275a14f",
-  season: "d41e8c26b592af575100fc48a7e",
-  episode: "8b7d132fe629c94541d8e7bc2",
-  imdbId: "6e2af5c974d219840b631a6d54",
+  id: "c81f42d9e22542643316f408",
+  fToken: "9e3c7b134a3f457652481d0e493",
+  ts: "54d8b2681fa337e260b1fd",
+  token: "b7f18e34c225d63aef81c4a9",
+  title: "2af9c71de33228480546391e",
+  year: "f0b34e866a232909275a14f",
+  season: "d41e8c26b592af575130fc48a7e",
+  episode: "8b7d132fe6239c94541d8e7bc2",
+  imdbId: "6e2af5c9734d219840b631a6d54",
   path: "7f4a91c63e285b917d4c2",
-  mediaType: "a6e42c91f7385d204b17e",
-  date: "3b91e7d42c685f109a36d",
+  mediaType: "a6e432c91f7385d204b17e",
+  date: "3b91e7d42c685f3109a36d",
 } as const;
 export { FIELD_MAP };
 
