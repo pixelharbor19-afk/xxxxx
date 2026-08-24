4986d7d47fc948d76a92cf4e5f743c98ef004a4f
jerometecson-main <zxctest8@zxcstream.xyz>
2026-08-24 19:43:35 +0800

    more berkas workers and rotate token

--- hooks/fetch-embed.ts
+++ hooks/fetch-embed.ts
@@ -24,7 +24,7 @@ async function fetchSentinelEmbed({
 }: FetchEmbedParams) {
   const { xt, rt } = generateFrontendToken(tmdbId);
 
-  const backendRes = await axios.post("/backend_/token", {
+  const backendRes = await axios.post("/backend_/dynamic", {
     [FIELD_MAP.id]: tmdbId,
     [FIELD_MAP.fToken]: xt,
     [FIELD_MAP.ts]: rt,
--- hooks/subs.ts
+++ hooks/subs.ts
@@ -34,7 +34,7 @@ export default function useSubtitle({
     refetchOnReconnect: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend_/token", {
+      const { data: token } = await axios.post("/backend_/dynamic", {
         [FIELD_MAP.id]: tmdbId,
         [FIELD_MAP.mediaType]: media_type,
         [FIELD_MAP.path]: "subtitle_",
