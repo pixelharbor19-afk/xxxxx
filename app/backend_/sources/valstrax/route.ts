f4d3fa441f6463dcf5ea2ea409e5fc441e083410
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-06 09:07:27 +0800

    add cache

--- app/backend_/sources/valstrax/route.ts
+++ app/backend_/sources/valstrax/route.ts
@@ -158,7 +158,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { fetchWithTimeout } from "@/lib/fetch-timeout";
 import { encryptUrl } from "@/lib/aes-encryptor";
 import { encryptLink } from "@/lib/source-link-enc-dec";
-import { FIELD_MAP } from "@/lib/field-map";
+import { FIELD_MAP } from "@/lib/params";
 import { validateBackendToken } from "@/lib/validate-token";
 import { isValidReferer } from "@/lib/allowed-referers";
 import { logRequest } from "@/lib/log-request";
