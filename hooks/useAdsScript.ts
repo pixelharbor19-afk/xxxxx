132964fcef91257b2bb90d822b60c2e132238232
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-16 23:51:22 +0800

    update ad link

--- hooks/useAdsScript.ts
+++ hooks/useAdsScript.ts
@@ -23,7 +23,8 @@ export function useAdsScript({ enabled, platform }: UseAdsScriptProps) {
           "https://injusticebakery.com/5c/15/e7/5c15e7185944758aafe9b32aa87f5279.js";
       } else {
         script = document.createElement("script");
-        script.src = "//em.barresrelists.com/ryrcYDpZjtng/oLqVX";
+        // script.src = "//em.barresrelists.com/ryrcYDpZjtng/oLqVX";
+        script.src = "//us.rsumgiros.com/rOu82bXm2U2M3afp/oLqVX";
         script.setAttribute("data-cfasync", "false");
       }
     } else if (host === "zxcprime.xyz" || host.endsWith(".zxcprime.xyz")) {
