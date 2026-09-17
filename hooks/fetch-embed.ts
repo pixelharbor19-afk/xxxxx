1d90556e455c24cc9b5dc282910d32e9db8034ce
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-17 08:25:56 +0800

    fix valstrax

--- hooks/fetch-embed.ts
+++ hooks/fetch-embed.ts
@@ -24,7 +24,7 @@ async function fetchSentinelEmbed({
 }: FetchEmbedParams) {
   const { xt, rt } = generateFrontendToken(tmdbId);
 
-  const backendRes = await axios.post("/backend/wtf", {
+  const backendRes = await axios.post("/backend/whattheactualfuck", {
     [FIELD_MAP.id]: tmdbId,
     [FIELD_MAP.fToken]: xt,
     [FIELD_MAP.ts]: rt,
