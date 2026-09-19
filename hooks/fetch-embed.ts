f4a66c612e31d3dcbc1c7ee3f21852dfae223a11
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-19 08:21:42 +0800

    zxc

--- hooks/fetch-embed.ts
+++ hooks/fetch-embed.ts
@@ -24,7 +24,7 @@ async function fetchSentinelEmbed({
 }: FetchEmbedParams) {
   const { xt, rt } = generateFrontendToken(tmdbId);
 
-  const backendRes = await axios.post("/backend/abaygagoka", {
+  const backendRes = await axios.post("/backend/putanginamoba", {
     [FIELD_MAP.id]: tmdbId,
     [FIELD_MAP.fToken]: xt,
     [FIELD_MAP.ts]: rt,
--- hooks/subs.ts
+++ hooks/subs.ts
@@ -34,7 +34,7 @@ export default function useSubtitle({
     refetchOnReconnect: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend/abaygagoka", {
+      const { data: token } = await axios.post("/backend/putanginamoba", {
         [FIELD_MAP.id]: tmdbId,
         [FIELD_MAP.mediaType]: media_type,
         [FIELD_MAP.path]: "subtitle_",
