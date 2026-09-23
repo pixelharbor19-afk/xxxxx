105a60c61153b774a65eed22a5a72b0e6a82b72f
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-24 06:26:44 +0800

    adjust

--- app/backend_/sources/valstrax/route.ts
+++ app/backend_/sources/valstrax/route.ts
@@ -219,11 +219,11 @@ export async function GET(req: NextRequest) {
       .eq("media_type", mediaType)
       .eq("season", season)
       .eq("episode", episode)
-      .gte(
-        "expires_at",
-        new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString(),
-      )
-      // .gt("expires_at", new Date().toISOString())
+      // .gte(
+      //   "expires_at",
+      //   new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString(),
+      // )
+      .gt("expires_at", new Date().toISOString())
       .maybeSingle();
 
     if (cached) {
