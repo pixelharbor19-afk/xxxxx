09b6bdad082905555a6f78b6929e796db4937605
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-25 19:42:56 +0800

    rotate

--- hooks/subs.ts
+++ hooks/subs.ts
@@ -34,7 +34,7 @@ export default function useSubtitle({
     refetchOnReconnect: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend/ololmo", {
+      const { data: token } = await axios.post("/backend_/bugokkaba", {
         [FIELD_MAP.id]: tmdbId,
         [FIELD_MAP.mediaType]: media_type,
         [FIELD_MAP.path]: "subtitle_",
