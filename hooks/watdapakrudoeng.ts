2406eede6cbbcbd62a7b8e8949db7d215cc8bf96
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-26 22:44:19 +0800

    rotate

--- hooks/watdapakrudoeng.ts
+++ hooks/watdapakrudoeng.ts
@@ -96,7 +96,7 @@ export default function useSource(params: UseSourceParams) {
     refetchIntervalInBackground: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend_/whattheheal", {
+      const { data: token } = await axios.post("/backend_/kinanginamo", {
         [FIELD_MAP.id]: tmdbId,
         [FIELD_MAP.mediaType]: media_type,
         [FIELD_MAP.path]: path,
--- lib/params.ts
+++ lib/params.ts
@@ -4,19 +4,19 @@ import { SALT } from "./salt";
 // 🔁 Rotate these constants every few weeks
 
 const FIELD_MAP = {
-  id: "a7f39c821d604e5b9c7143f36e1547b",
-  fToken: "e83c4b719a52d8f3136052479c1635a",
-  ts: "61d9a5274c8e3b29af75d6384c291e6",
+  id: "a7f39c821d604e5b9c71f36e1547b",
+  fToken: "e83c4b719a52d3136052479c1635a",
+  ts: "61d9a5274c8e3b29afd6384c291e6",
   token: "c492f7a183d6502b1e7436c538a716d",
-  title: "5e28c9147a306d531e829f3674b392a1",
-  year: "b731e6c94f082a169d725f8341c306e",
+  title: "5e28c9147a306d1e829f3674b392a1",
+  year: "b731e6c94f08269d725f8341c306e",
   season: "d8427b59ce30684a2f957c3613e85b",
-  episode: "91c6e4a728bd503d1f785c92346b713d",
+  episode: "91c6e4a728503d1f785c92346b713d",
   imdbId: "f35a8c19d674b3265e871c4933a725f",
-  path: "6b491e7253ad8f14d392e7561a9384c",
-  mediaType: "c285f91ab306d281e947a35632e816b",
-  date: "e164932c50216ad739e5814b3027",
-  latestDate: "e16932c54356416ad739e5814b3027",
+  path: "6b491e7253ad84d392e7561a9384c",
+  mediaType: "c285f91ab306d28147a35632e816b",
+  date: "e164932c50216a39e5814b3027",
+  latestDate: "e16932c543416ad739e5814b3027",
 } as const;
 
 export { FIELD_MAP };
